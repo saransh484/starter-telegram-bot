9b7d9039bf7fca427edab46cfc5dbf9dec951070
saransh484 <saransh8@hotmail.com>
2022-11-30 09:13:48 +0530

    edited token

--- src/bot.ts
+++ src/bot.ts
@@ -6,7 +6,7 @@ import { applyTextEffect, Variant } from "./textEffects";
 import type { Variant as TextEffectVariant } from "./textEffects";
 
 // Create a bot using the Telegram token
-const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+const bot = new Bot(process.env.TELEGRAM_TOKEN || "5766277915:AAGar84gExAFubzL3-5Yu-PoiCyoNGd8UWE");
 
 // Handle the /yo command to greet the user
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
